b25862c7508d224efe82344c283a9096b6e7d3ab
Delva Patrick <delvapatrick25@gmail.com>
2025-12-16 17:27:34 -0500

    Creation de la Classe BankAccount avec ces Attributs et Methodes

---
+++ src/main/java/ht/ueh/first/java/BankAccount.java
@@ -0,0 +1,33 @@
+package ht.ueh.first.java;
+
+public class BankAccount {
+
+    private int accountNumber;
+    private double balance;
+
+
+    public BankAccount (int accountNumber, double balance) {
+
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void  deposit(double amount){
+        balance += amount;
+        System.out.println("Dépôt de " + amount + " $ | Nouveau solde : " + balance);
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Retrait de " + amount + " $ | Nouveau solde : " + balance);
+        } else {
+            System.out.println("Retrait refusé : solde insuffisant");
+        }
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+}
