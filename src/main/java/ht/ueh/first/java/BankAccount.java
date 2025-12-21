386c93fdbcc5d835135041d41318d8fa7a289a8b
zamormp-collab <zamormp@gmail.com>
2025-12-21 17:51:27 -0500

    Améliorer les messages d'exception dans la classe BankAccount
    Mise à jour des messages d'exception pour les méthodes de dépôt et de retrait pour fournir des commentaires plus clairs aux utilisateurs.

--- src/main/java/ht/ueh/first/java/BankAccount.java
+++ src/main/java/ht/ueh/first/java/BankAccount.java
@@ -14,28 +14,22 @@ public class BankAccount {
 
     public synchronized void deposit(double amount) throws InvalidAmountException{
         if(amount<=0){
-            throw new InvalidAmountException ("Le Montant" + amount +"est invalide, les valeurs negatives ne sont pas acceptees");
+            throw new InvalidAmountException (" Veuillez entrer une valeur supérieure à zéro.\n");
         } 
         balance += amount;
-        System.out.println("Dépôt de " + amount + " € | Nouveau solde : " + balance);
+        System.out.println("Dépôt de " + amount + " € | Nouveau solde : " + balance + " €.\n");
     }
 
     public synchronized void withdraw(double amount) throws InsufficientFundsException, InvalidAmountException{
         if(amount<=0){
-            throw new InvalidAmountException(
-                    "Montant de retrait invalide : " + amount
-            );
-        }
+            throw new InvalidAmountException (" Vous ne pouvez pas retirer ce montant de " + amount + " €, entrez une valeur supérieure ou égale à zéro.\n");
+        }  
 
         if(amount>balance){
-            throw new InsufficientFundsException(
-                    "Fonds insuffisants : solde = " + balance
-                            + ", retrait demandé = " + amount
-            );        }
-
+            throw new InsufficientFundsException (" Veuillez entrer une valeur inférieure à " + balance + " €.\n");
+        }
         balance -= amount;
-        System.out.println(Thread.currentThread().getName()
-                + " | Retrait de " + amount + " € | Solde : " + balance);
+        System.out.println("TRANSACTION RÉUSSIE : \nUn montant de " + amount + " € a été retiré.");
     }
 
     public synchronized double getBalance(){
