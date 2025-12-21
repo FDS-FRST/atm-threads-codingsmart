ed93247eb97d09825e5d5600494cb0bdc0f8c1e7
zamormp-collab <zamormp@gmail.com>
2025-12-21 15:46:57 -0500

    Capture des exceptions a l'execution des threads
    Capture des exceptions a l'execution des threads dans la methode run

--- src/main/java/ht/ueh/first/java/ATMTask.java
+++ src/main/java/ht/ueh/first/java/ATMTask.java
@@ -1,5 +1,8 @@
 package ht.ueh.first.java;
 
+import java.io.IOException;
+
+
 public class ATMTask implements Runnable {
 
     public enum Action {
@@ -11,33 +14,63 @@ public class ATMTask implements Runnable {
     private BankAccount account;
     private Action action;
     private double amount;
+    private int idClient;
 
-    public ATMTask(BankAccount account, Action action, double amount) {
+    public ATMTask(BankAccount account, Action action, double amount, int IdClient){
         this.account = account;
         this.action = action;
         this.amount = amount;
+        this.idClient = idClient;
     }
 
     @Override
     public void run() {
-    try {
-        switch (action) {
-            case DEPOSIT:
-                account.deposit(amount);
-                break;
-            case WITHDRAW:
-                account.withdraw(amount);
-                break;
-            case BALANCE:
-                System.out.println(Thread.currentThread().getName()
-                        + " | Solde : " + account.getBalance() + " €");
-                break;
+        try {
+            switch (action) {
+                case Action.DEPOSIT:
+                    account.deposit(amount);
+                    System.out.println(log("Dépôt effectué avec succès : " + amount));
+                    break;
+                case WITHDRAW:
+                    account.withdraw(amount);
+                    System.out.println(log("Retrait effectué avec succès : " + amount));
+                    break;
+                case BALANCE:
+                    System.out.println(Thread.currentThread().getName()
+                            + " | La balance du compte est : " + account.getBalance() + " €");
+                    System.out.println(log("Balance consultée avec succès"));
+                    break;
+                default:
+                    //Capture de l'exception au niveau du thread pour ne pas remonter dans Main et impacter le thread principal
+                    throw new IllegalArgumentException("ACTION HORS CONTEXTE");
+            }
         }
-    } catch (InvalidAmountException | InsufficientFundsException e) {
-       System.out.println(e.getMessage());
-    } finally {
+        catch (IllegalArgumentException e) {
+                Thread.currentThread().interrupt();
+                System.err.println(log("Interruption du thread"));
+        }
+        catch(InvalidAmountException e){
+            System.err.println(log("La tentative de dépot de " + amount + " € a échouée, car le montant entré est inférieure ou égale à zéro" + e.getMessage()));
+        }
+        catch(InsufficientFundsException e){
+            System.err.println(log("La tentative de retrait de " + amount + " € a échouée, car votre balance n'est pas suffisante" + e.getMessage()));
+        }
+            finally{
+                System.out.println();
+            }
 
+        try {
+            Thread.sleep(500);
+        }
+            catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Interruption du thread : " + Thread.currentThread().getName());
+            }
     }
 
+    private String log(String message){
+        return "[Client " + idClient +
+                " | Thread " + Thread.currentThread().getName() +
+                "] " + message;
     }
 }
